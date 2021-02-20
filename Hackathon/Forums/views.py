c6d93d9c41cfd44e3216b2214467fc1476e967e3
James Hariady Widjaja <widjajajames01@gmail.com>
2021-02-20 07:38:56 -0800

    added test data

--- Hackathon/Forums/views.py
+++ Hackathon/Forums/views.py
@@ -5,11 +5,11 @@ from .models import Topic, Subtopic
 # Create your views here.
 def topic_list_view(request):
     topic_list = Topic.objects.order_by("pub_date")
-    return render(request, 'forum/topic_list.html', {"topic_list": topic_list})
+    return render(request, 'Forum/forums.html', {"topic_list": topic_list})
 
 
 def topic_view(request, topic_id):
     topic = get_object_or_404(Topic, pk=topic_id)
     subtopics = topic.subtopic_set.all()
-    return render(request, 'forum.topic_view.html', {"topic": topic,
+    return render(request, 'Forum.topic_view.html', {"topic": topic,
                                                      "subtopics": subtopics})
