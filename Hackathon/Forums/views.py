7ed80bc245114b144e1ad882f2e2a3a7aeee2da8
James Hariady Widjaja <widjajajames01@gmail.com>
2021-02-20 00:26:19 -0800

    added views

--- Hackathon/Forums/views.py
+++ Hackathon/Forums/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Topic, Subtopic
+
 
 # Create your views here.
-def render_forums(request):
-    return render(request, 'Forums/forums.html')
+def topic_list_view(request):
+    topic_list = Topic.objects.order_by("pub_date")
+    return render(request, 'forum/topic_list.html', {"topic_list": topic_list})
+
+
+def topic_view(request, topic_id):
+    topic = get_object_or_404(Topic, pk=topic_id)
+    subtopics = topic.subtopic_set.all()
+    return render(request, 'forum.topic_view.html', {"topic": topic,
+                                                     "subtopics": subtopics})
