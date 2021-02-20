a64c75eab607eee28d99c640ed28a3a13c164922
Michael <mjchan2000@gmail.com>
2021-02-20 16:48:08 -0500

    Updating Hackathon

--- Hackathon/Hackathon/urls.py
+++ Hackathon/Hackathon/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('Community_Page.urls')),
     path('admin/', admin.site.urls),
     path('', include('Home_Page.urls')),
     path('forums/', include('Forums.urls')),
