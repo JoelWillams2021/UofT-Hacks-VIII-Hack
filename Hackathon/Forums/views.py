dc47c0c669021b441b46e215782361cca55940b6
James Hariady Widjaja <widjajajames01@gmail.com>
2021-02-20 19:20:36 -0800

    some changes

--- Hackathon/Forums/views.py
+++ Hackathon/Forums/views.py
@@ -21,8 +21,12 @@ def render_forums_view(request):
 def render_topic_view(request, topic_id):
     topic = get_object_or_404(Topic, pk=topic_id)
     subtopics = topic.subtopic_set.all()
+    suggestions_dict = {}
+    for subtopic in subtopics:
+        suggestions_dict[subtopic] = subtopic.suggestion_set.all()
     return render(request, 'Forums/topic_view.html', {"topic": topic,
-                                                      "subtopics": subtopics})
+                                                      "subtopics": subtopics,
+                                                      "suggestions_dict": suggestions_dict})
 
 
 def render_subtopic_view(request, subtopic_id):
